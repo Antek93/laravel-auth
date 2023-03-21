9a333e4a83fb55f4c4a3738f7123a85dae0c5155
Antek93 <antonio.savino1993@gmail.com>
2023-03-21 21:59:58 +0100

    aggiunti elementi grafici

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -3,6 +3,7 @@
 @section('content')
     <div class="container-fluid mt-4">
         <div class="row justify-content-center">
+
             <div class="col-12">
                 <h1>
                     Tutti i miei progetti
@@ -19,13 +20,11 @@
             </div>
             @foreach ($projects as $project)
                 <div class="row">
-                    <div class="col-2">
-                        <div class="fw-bold ">
-                            <hr>
-                            <h2>
-                                {{ $project->id }}) {{ $project->name }}
-                            </h2>
-                        </div>
+                    <div class="fw-bold ">
+                        <hr>
+                        <h2>
+                            {{ $project->id }}) {{ $project->name }}
+                        </h2>
                     </div>
                     <div class="d-flex justy-content-between">
                         <div>
@@ -49,7 +48,6 @@
                         </div>
                     </div>
             @endforeach
-
         </div>
     </div>
     </div>
